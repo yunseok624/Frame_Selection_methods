9f9be6076557455d8d8d95dc28c6f539aec4bca4
yunseok624 <parkyunseok624@gmail.com>
2026-03-06 15:57:17 +0900

    fix: bypass .to() error for 8-bit quantization to avoid OOM

--- lmms-eval/lmms_eval/models/llava_vid.py
+++ lmms-eval/lmms_eval/models/llava_vid.py
@@ -224,7 +224,7 @@ class LlavaVid(lmms):
             self._word_size = 1
         else:
             eval_logger.info(f"Using single device: {self._device}")
-            self.model.to(self._device)
+            # self.model.to(self._device)
             self._rank = 0
             self._world_size = 1
 
