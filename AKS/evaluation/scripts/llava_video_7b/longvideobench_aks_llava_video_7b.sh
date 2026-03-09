3583238e3c106dffe7893d6d99932e5546d6c42a
yunseok624 <parkyunseok624@gmail.com>
2026-03-09 15:50:51 +0900

    4bit quantization + 32 frames + flash-attention

--- AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
+++ AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
@@ -1,7 +1,7 @@
 base_score_path=./selected_frames/longvideobench/clip
 score_type=selected_frames
 dataset_name=longvideobench
-frame_num=64
+frame_num=32
 
 python ./evaluation/change_score.py \
     --base_score_path $base_score_path \
@@ -11,7 +11,7 @@ python ./evaluation/change_score.py \
 
 CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes 8 --main_process_port 12345 -m lmms_eval \
     --model llava_vid \
-    --model_args pretrained=/content/drive/MyDrive/checkpoints/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=64,overwrite=False,use_topk=True\
+    --model_args pretrained=/content/drive/MyDrive/checkpoints/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=True,attn_implementation=flash_attention_2\
     --tasks longvideobench_val_v \
     --batch_size 1 \
     --log_samples \
