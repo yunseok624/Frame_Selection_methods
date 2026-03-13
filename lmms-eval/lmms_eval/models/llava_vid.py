e3b9f2d84cd294e31bc04d0cc275cf64b7f1cf14
yunseok624 <parkyunseok624@gmail.com>
2026-03-13 17:18:47 +0900

    added kv cache

--- lmms-eval/lmms_eval/models/llava_vid.py
+++ lmms-eval/lmms_eval/models/llava_vid.py
@@ -614,6 +614,7 @@ class LlavaVid(lmms):
                     attention_mask=attention_masks,
                     modalities="video",
                     use_cache=self.use_cache,
+                    cache_implementation="quantized", # added 8bit kv cache
                     stopping_criteria=[stopping_criteria],
                     do_sample=True if gen_kwargs["temperature"] > 0 else False,
                     temperature=gen_kwargs["temperature"],
