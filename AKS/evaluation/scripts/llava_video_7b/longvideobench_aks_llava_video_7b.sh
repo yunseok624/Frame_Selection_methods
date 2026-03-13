cfa86ba9fb65acde20a54872dd531759cd7fad60
yunseok624 <parkyunseok624@gmail.com>
2026-03-13 17:16:23 +0900

    changed the path for kaggle environment

--- AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
+++ AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
@@ -9,11 +9,11 @@ python ./evaluation/change_score.py \
     --dataset_name $dataset_name \
     --num_frames $frame_num
 
-CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes 1 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval \
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --num_processes 2 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval \
     --model llava_vid \
-    --model_args pretrained=/content/drive/MyDrive/checkpoints/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=True,attn_implementation=sdpa\
+    --model_args pretrained=/kaggle/working/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=True,attn_implementation=sdpa\
     --tasks longvideobench_val_v \
     --batch_size 1 \
     --log_samples \
     --log_samples_suffix llavavid_7b_qwen_lvb_v \
-    --output_path /content/drive/MyDrive/results/${dataset_name}_aks_${frame_num}
+    --output_path /kaggle/working/results/${dataset_name}_aks_${frame_num}
--- AKS/evaluation/scripts/llava_video_7b/longvideobench_uni_llava_video_7b.sh
+++ AKS/evaluation/scripts/llava_video_7b/longvideobench_uni_llava_video_7b.sh
@@ -3,9 +3,9 @@ frame_num=32
 
 CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes 1 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval\
     --model llava_vid \
-    --model_args pretrained=/content/drive/MyDrive/checkpoints/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=64,overwrite=False,use_topk=False,,attn_implementation=sdpa\
+    --model_args pretrained=/kaggle/working/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=64,overwrite=False,use_topk=False,,attn_implementation=sdpa\
     --tasks longvideobench_val_v \
     --batch_size 1 \
     --log_samples \
     --log_samples_suffix llavavid_7b_qwen_lvb_v \
-    --output_path /content/drive/MyDrive/results/${score_type}_${frame_num}
+    --output_path /kaggle/working/results/${score_type}_${frame_num}
