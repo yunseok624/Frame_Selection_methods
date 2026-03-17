6e2b7cf7f9bc36f944c5282796aac6cd35306551
yunseok624 <parkyunseok624@gmail.com>
2026-03-17 09:11:48 +0900

    changed the path for kaggle environment

--- AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
+++ AKS/evaluation/scripts/llava_video_7b/longvideobench_aks_llava_video_7b.sh
@@ -11,7 +11,7 @@ python ./evaluation/change_score.py \
 
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --num_processes 2 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval \
     --model llava_vid \
-    --model_args pretrained=/kaggle/working/LLaVA-NeXT-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=True,attn_implementation=sdpa\
+    --model_args pretrained=/kaggle/input/models/yunseok624/llava-next-video-7b-qwen2/transformers/default/1/LLaVA-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=True,attn_implementation=sdpa\
     --tasks longvideobench_val_v \
     --batch_size 1 \
     --log_samples \
