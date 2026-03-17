5fc0af082af922eaa8b6fb19a720a59e953952f8
yunseok624 <parkyunseok624@gmail.com>
2026-03-17 17:00:31 +0900

    changed num_processes for uniform sampling

--- AKS/evaluation/scripts/llava_video_7b/longvideobench_uni_llava_video_7b.sh
+++ AKS/evaluation/scripts/llava_video_7b/longvideobench_uni_llava_video_7b.sh
@@ -1,7 +1,7 @@
 score_type=longvideobench_uni
 frame_num=32
 
-CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes 1 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval\
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --num_processes 2 --mixed_precision fp16 --main_process_port 12345 -m lmms_eval\
     --model llava_vid \
     --model_args pretrained=/kaggle/temp/LLaVA-Video-7B-Qwen2,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=32,overwrite=False,use_topk=False,attn_implementation=sdpa\
     --tasks longvideobench_val_v \
