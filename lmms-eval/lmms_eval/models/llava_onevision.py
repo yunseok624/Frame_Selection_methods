7bd61c0c157be224445f5a2dbff7d56f10bd6107
yunseok624 <parkyunseok624@gmail.com>
2026-03-18 13:48:09 +0900

    added 4bit quantization to llava_onevision

--- lmms-eval/lmms_eval/models/llava_onevision.py
+++ lmms-eval/lmms_eval/models/llava_onevision.py
@@ -133,11 +133,11 @@ class Llava_OneVision(lmms):
         llava_model_args["overwrite_config"] = overwrite_config
         try:
             # Try to load the model with the multimodal argument
-            self._tokenizer, self._model, self._image_processor, self._max_length = load_pretrained_model(pretrained, None, model_name, device_map=self.device_map, **llava_model_args)
+            self._tokenizer, self._model, self._image_processor, self._max_length = load_pretrained_model(pretrained, None, model_name, load_4bit=True, device_map=self.device_map, **llava_model_args)
         except TypeError:
             # for older versions of LLaVA that don't have multimodal argument
             llava_model_args.pop("multimodal", None)
-            self._tokenizer, self._model, self._image_processor, self._max_length = load_pretrained_model(pretrained, None, model_name, device_map=self.device_map, **llava_model_args)
+            self._tokenizer, self._model, self._image_processor, self._max_length = load_pretrained_model(pretrained, None, model_name, load_4bit=True, device_map=self.device_map, **llava_model_args)
 
         self._config = self._model.config
         self.model.eval()
