9eda44c4fa760ecea413ee961a9ff962ffc283a0
yunseok624 <parkyunseok624@gmail.com>
2026-03-18 13:55:40 +0900

    added kv cache to llava_onevision

--- lmms-eval/lmms_eval/models/llava_onevision.py
+++ lmms-eval/lmms_eval/models/llava_onevision.py
@@ -560,7 +560,7 @@ class Llava_OneVision(lmms):
                 gen_kwargs.pop("image_aspect_ratio")
             try:
                 with torch.inference_mode():
-                    cont = self.model.generate(input_ids, attention_mask=attention_masks, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, **gen_kwargs)
+                    cont = self.model.generate(input_ids, attention_mask=attention_masks, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, cache_implementation="quantized", **gen_kwargs)
                     # cont = self.model.generate(qwen_input_ids, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, **gen_kwargs)
 
                 text_outputs = self.tokenizer.batch_decode(cont, skip_special_tokens=True)
@@ -764,7 +764,7 @@ class Llava_OneVision(lmms):
                     gen_kwargs.pop("image_aspect_ratio")
                 try:
                     with torch.inference_mode():
-                        cont = self.model.generate(input_ids, attention_mask=attention_masks, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, **gen_kwargs)
+                        cont = self.model.generate(input_ids, attention_mask=attention_masks, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, cache_implementation="quantized", **gen_kwargs)
                         # cont = self.model.generate(qwen_input_ids, pad_token_id=pad_token_ids, images=image_tensor, use_cache=self.use_cache, **gen_kwargs)
 
                     text_outputs = self.tokenizer.batch_decode(cont, skip_special_tokens=True)
